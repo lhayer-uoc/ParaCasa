5105efe881adf7bcafbe7fcfe2108b566d18b72c
Luciano Hayer <lucianohayer@gmail.com>
2022-03-16 20:26:40 +0100

    Initial commit

---
+++ src/main/java/com/javack/ParaCasa/ParaCasaApplication.java
@@ -0,0 +1,13 @@
+package com.javack.ParaCasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaCasaApplication.class, args);
+	}
+
+}
