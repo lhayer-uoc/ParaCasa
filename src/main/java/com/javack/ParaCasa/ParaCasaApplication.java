ef455a1830a36ea7e9b4fe889eb428479f71639f
iago0 <posseiago@gmail.com>
2022-04-21 02:40:44 +0200

    spring security con loggin

--- src/main/java/com/javack/ParaCasa/ParaCasaApplication.java
+++ src/main/java/com/javack/ParaCasa/ParaCasaApplication.java
@@ -1,15 +1,19 @@
 package com.javack.ParaCasa;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class ParaCasaApplication {
+public class ParaCasaApplication{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParaCasaApplication.class, args);
 	}
+
+		
 	
-	//prueba escritura
 
 }
---
+++ src/main/java/com/javack/ParaCasa/WebSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.javack.ParaCasa;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private BCryptPasswordEncoder passEncoder;
+	
+	
+	
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/", "/home", "/index", "/css/**", "/js/**", "/images/**").permitAll()
+		.antMatchers("/views/menus/").hasAnyRole("USER")
+		.antMatchers("/views/menus/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/menus/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/menus/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/menus/delete/**").hasAnyRole("ADMIN")
+		
+		.antMatchers("/views/pedidos/").hasAnyRole("USER")
+		//Los pedidos pueden ser creados y guardados también por usuarios
+		.antMatchers("/views/pedidos/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/pedidos/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/pedidos/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/pedidos/delete/**").hasAnyRole("ADMIN")
+		
+		
+		.antMatchers("/views/productos/").hasAnyRole("USER")
+		.antMatchers("/views/productos/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/productos/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/productos/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/productos/delete/**").hasAnyRole("ADMIN")
+		
+		
+		.antMatchers("/views/tipos/").hasAnyRole("USER")
+		.antMatchers("/views/tipos/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/tipos/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/tipos/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/tipos/delete/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login")
+		.permitAll()
+		.and()
+		.logout().permitAll();
+	}
+
+
+
+
+
+	//Comprobamos que el usuario exista
+	@Autowired
+	public void configurerSecurityGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		builder.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passEncoder)
+		.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+		.authoritiesByUsernameQuery("SELECT u.username, r.rol FROM roles r INNER JOIN users u ON r.user_id=u.id WHERE u.username=?");
+	}
+	
+}
