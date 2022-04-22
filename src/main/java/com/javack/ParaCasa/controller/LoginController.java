33214010de97d9d9eeb2e5a6fa618729594d003e
iago0 <posseiago@gmail.com>
2022-04-22 19:59:01 +0200

    security todo funcional

--- src/main/java/com/javack/ParaCasa/controller/LoginController.java
+++ src/main/java/com/javack/ParaCasa/controller/LoginController.java
@@ -1,13 +1,32 @@
 package com.javack.ParaCasa.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String login() {
+	public String login(@RequestParam(value="error", required=false) String error,
+			@RequestParam(value="logout", required=false) String logout,
+			Model model, Principal principal, RedirectAttributes attribute) {
+		if(error != null) {
+			model.addAttribute("error", "Error de acceso: Usuario y/o contraseña incorrectos");
+		}
+		
+		if(principal != null) {
+			attribute.addFlashAttribute("warning", "ATENCION, ya ha iniciado sesión");
+					return "redirect:/home";
+		}
+		
+		if(logout != null) {
+			model.addAttribute("success", "HA FINALIZADO SESION CON EXITO");
+		}
 		return "login";
 	}
 }
