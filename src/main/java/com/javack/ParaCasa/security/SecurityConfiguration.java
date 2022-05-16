25aa37e2fbc0300103cfdfbc42a15a74cefe4cb8
iago0 <posseiago@gmail.com>
2022-05-16 20:23:36 +0200

    cosas que me quedaron sin guardar

--- src/main/java/com/javack/ParaCasa/security/SecurityConfiguration.java
+++ src/main/java/com/javack/ParaCasa/security/SecurityConfiguration.java
@@ -44,6 +44,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().
 		antMatchers("/", "/home", "/index","/registro**", "/css/**", "/js/**", "/images/**" ).permitAll()
+		.antMatchers("/views/menus/").hasAnyRole("USER")
+		.antMatchers("/views/menus/create").hasAnyRole("ROLE_ADMIN")
+		.antMatchers("/views/menus/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/menus/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/menus/delete/**").hasAnyRole("ADMIN")
+		
+		.antMatchers("/views/pedidos/").hasAnyRole("USER")
+		//Los pedidos pueden ser creados y guardados también por usuarios
+		.antMatchers("/views/pedidos/create").hasAnyRole("USER")
+		.antMatchers("/views/pedidos/save").hasAnyRole("USER")
+		.antMatchers("/views/pedidos/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/pedidos/delete/**").hasAnyRole("ADMIN")
+		
+		
+		.antMatchers("/views/productos/").hasAnyRole("USER")
+		.antMatchers("/views/productos/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/productos/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/productos/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/productos/delete/**").hasAnyRole("ADMIN")
+		
+		
+		.antMatchers("/views/tipos/").hasAnyRole("USER")
+		.antMatchers("/views/tipos/create").hasAnyRole("ADMIN")
+		.antMatchers("/views/tipos/save").hasAnyRole("ADMIN")
+		.antMatchers("/views/tipos/edit/**").hasAnyRole("ADMIN")
+		.antMatchers("/views/tipos/delete/**").hasAnyRole("ADMIN")
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
